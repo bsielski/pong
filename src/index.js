1db637b5be8f242f3039b83f078c00bb9007c4ff
Strategiusz <strategiusz.yt@gmail.com>
2018-03-04 17:10:06 +0100

    Hello World with pixi.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import * as PIXI from 'pixi.js'
 
 function runGame() {
   class Dog {
@@ -14,6 +15,27 @@ function runGame() {
   loltest();
   const dog = new Dog();
   dog.bark();
+
+  let app = new PIXI.Application({
+      width: 256,
+      height: 256,
+      antialias: true,
+      transparent: false,
+      resolution: 1
+    }
+  );
+
+  document.body.appendChild(app.view);
+
+  PIXI.loader
+    .add("skull.png")
+    .load(setup);
+
+  function setup() {
+    let skull = new PIXI.Sprite(PIXI.loader.resources["skull.png"].texture);
+    app.stage.addChild(skull);
+  }
+
 }
 
 window.addEventListener('load', runGame);
