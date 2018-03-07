b4dc3dfa499ce40a8c7ffa3070a80ff096bffbed
Strategiusz <strategiusz.yt@gmail.com>
2018-03-07 10:51:05 +0100

    Group code by entities instead of type of components

--- src/components.js
+++ src/components.js
@@ -1,27 +1,38 @@
 import Config from './config';
 
 const Components = {
-  bodies: {
-    1: {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, static: true},
-    2: {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, static: true},
-    3: {width: Config.BALL_WIDTH, height: Config.BALL_HEIGHT, static: false},
-    4: {width: 20, height: Config.WORLD_HEIGHT, static: true},
-    5: {width: 20, height: Config.WORLD_HEIGHT, static: true},
-    6: {width: Config.WORLD_WIDTH, height: 20, static: true},
-    7: {width: Config.WORLD_WIDTH, height: 20, static: true},
-  },
-  positions: {
-    1: {x: Config.PADDLE_1_POSITION.X, y: Config.PADDLE_1_POSITION.Y},
-    2: {x: Config.PADDLE_2_POSITION.X, y: Config.PADDLE_2_POSITION.Y},
-    3: {x: Config.BALL_POSITION.X, y: Config.BALL_POSITION.Y},
-    4: {x: 10, y: Config.WORLD_HEIGHT/2},
-    5: {x: Config.WORLD_WIDTH - 10, y: Config.WORLD_HEIGHT/2},
-    6: {x: Config.WORLD_WIDTH/2, y: 10,                     },
-    7: {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 10,},
-  },
-  orders: {
-    2: {leftOrder: "stop", rightOrder: "stop"},
-  }
+  bodies: {},
+  positions: {},
+  orders: {},
 }
+
+const enemyPaddle = 1;
+Components.bodies[enemyPaddle] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, static: true};
+Components.positions[enemyPaddle] = {x: Config.PADDLE_1_POSITION.X, y: Config.PADDLE_1_POSITION.Y};
+
+const playerPaddle = 2;
+Components.bodies[playerPaddle] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, static: true};
+Components.positions[playerPaddle] = {x: Config.PADDLE_2_POSITION.X, y: Config.PADDLE_2_POSITION.Y};
+Components.orders[playerPaddle] = {leftOrder: "stop", rightOrder: "stop"};
+
+const ball = 3;
+Components.bodies[ball] = {width: Config.BALL_WIDTH, height: Config.BALL_HEIGHT, static: false};
+Components.positions[ball] = {x: Config.BALL_POSITION.X, y: Config.BALL_POSITION.Y};
+
+const leftWall = 4;
+Components.bodies[leftWall] = {width: 20, height: Config.WORLD_HEIGHT, static: true};
+Components.positions[leftWall] = {x: 10, y: Config.WORLD_HEIGHT/2};
+
+const rightWall = 5;
+Components.bodies[rightWall] = {width: 20, height: Config.WORLD_HEIGHT, static: true};
+Components.positions[rightWall] = {x: Config.WORLD_WIDTH - 10, y: Config.WORLD_HEIGHT/2};
+
+const toptWall = 6;
+Components.bodies[toptWall] = {width: Config.WORLD_WIDTH, height: 20, static: true};
+Components.positions[toptWall] = {x: Config.WORLD_WIDTH/2, y: 10};
+
+const bottomWall = 7;
+Components.bodies[bottomWall] = {width: Config.WORLD_WIDTH, height: 20, static: true};
+Components.positions[bottomWall] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 10};
 
 export default Components;
