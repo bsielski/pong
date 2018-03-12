5f8f047b35b9919d32f23366e251ba8272a5298e
Strategiusz <strategiusz.yt@gmail.com>
2018-03-12 12:53:17 +0100

    Redesing level

--- src/components.js
+++ src/components.js
@@ -32,24 +32,24 @@ Components.positions[ball] = {x: Config.BALL_POSITION.X, y: Config.BALL_POSITION
 Components.movements[ball] = {x: 0.1, y: 0.6};
 
 const leftWall = 4;
-Components.bodies[leftWall] = {width: 20, height: Config.WORLD_HEIGHT, type: "immobile"};
-Components.sprites[leftWall] = {width: 20, height: Config.WORLD_HEIGHT, image: LOLPIXELS, opacity: 1.0};
-Components.positions[leftWall] = {x: 10, y: Config.WORLD_HEIGHT/2};
+Components.bodies[leftWall] = {width: 40, height: Config.WORLD_HEIGHT, type: "immobile"};
+Components.sprites[leftWall] = {width: 40, height: Config.WORLD_HEIGHT, image: LOLPIXELS, opacity: 1.0};
+Components.positions[leftWall] = {x: 0, y: Config.WORLD_HEIGHT/2};
 
 const rightWall = 5;
-Components.bodies[rightWall] = {width: 20, height: Config.WORLD_HEIGHT, type: "immobile"};
-Components.sprites[rightWall] = {width: 20, height: Config.WORLD_HEIGHT, image: LOLPIXELS, opacity: 1.0};
-Components.positions[rightWall] = {x: Config.WORLD_WIDTH - 10, y: Config.WORLD_HEIGHT/2};
+Components.bodies[rightWall] = {width: 40, height: Config.WORLD_HEIGHT, type: "immobile"};
+Components.sprites[rightWall] = {width: 40, height: Config.WORLD_HEIGHT, image: LOLPIXELS, opacity: 1.0};
+Components.positions[rightWall] = {x: Config.WORLD_WIDTH, y: Config.WORLD_HEIGHT/2};
 
 const toptWall = 6;
-Components.bodies[toptWall] = {width: Config.WORLD_WIDTH, height: 20, type: "immobile"};
-Components.sprites[toptWall] = {width: Config.WORLD_WIDTH, height: 20, image: LOLPIXELS, opacity: 1.0};
-Components.positions[toptWall] = {x: Config.WORLD_WIDTH/2, y: 10};
+Components.bodies[toptWall] = {width: Config.WORLD_WIDTH, height: 40, type: "immobile"};
+Components.sprites[toptWall] = {width: Config.WORLD_WIDTH, height: 40, image: LOLPIXELS, opacity: 1.0};
+Components.positions[toptWall] = {x: Config.WORLD_WIDTH/2, y: 0};
 
 const bottomWall = 7;
-Components.bodies[bottomWall] = {width: Config.WORLD_WIDTH, height: 20, type: "immobile"};
-Components.sprites[bottomWall] = {width: Config.WORLD_WIDTH, height: 20, image: LOLPIXELS, opacity: 1.0};
-Components.positions[bottomWall] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 10};
+Components.bodies[bottomWall] = {width: Config.WORLD_WIDTH, height: 40, type: "immobile"};
+Components.sprites[bottomWall] = {width: Config.WORLD_WIDTH, height: 40, image: LOLPIXELS, opacity: 1.0};
+Components.positions[bottomWall] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT};
 
 const net = 8;
 Components.sprites[net] = {width: Config.WORLD_WIDTH, height: 10, image: LOLPIXELS, opacity: 0.2};
@@ -57,7 +57,7 @@ Components.positions[net] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT/2};
 
 const fpsCounter = 9;
 Components.texts[fpsCounter] = {size: 12, content: "fps", opacity: 0.6};
-Components.positions[fpsCounter] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT/2 -40};
+Components.positions[fpsCounter] = {x: Config.WORLD_WIDTH/6, y: Config.WORLD_HEIGHT/2};
 Components.rulesFps[fpsCounter] = true;
 
 // const invisibleObstacle = 10;
@@ -65,35 +65,35 @@ Components.rulesFps[fpsCounter] = true;
 // Components.positions[invisibleObstacle] = {x: Config.WORLD_WIDTH/2 - 100, y: Config.WORLD_HEIGHT/2};
 
 const skull1 = 11;
-Components.sprites[skull1] = {width: 150, height: 150, image: SKULL, opacity: 0.4};
-Components.positions[skull1] = {x: 100, y: 100};
+Components.sprites[skull1] = {width: 110, height: 110, image: SKULL, opacity: 0.4};
+Components.positions[skull1] = {x: 500, y: 140};
 
 const skull2 = 12;
-Components.sprites[skull2] = {width: 150, height: 150, image: SKULL, opacity: 0.4};
-Components.positions[skull2] = {x: 500, y: 100};
+Components.sprites[skull2] = {width: 110, height: 110, image: SKULL, opacity: 0.4};
+Components.positions[skull2] = {x: 500, y: 360};
 
 const bottomZone = 13;
 Components.bodies[bottomZone] = {width: Config.WORLD_WIDTH, height: 20, type: "zone"};
-Components.positions[bottomZone] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 15};
+Components.positions[bottomZone] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 25};
 Components.sensors[bottomZone] = {seeking: 3, detected: false};
 
 const bottomCounter = 14;
-Components.positions[bottomCounter] = {x: 50, y: 350};
-Components.texts[bottomCounter] = {size: 22, content: "0", opacity: 0.6};
+Components.positions[bottomCounter] = {x: 60, y: 280};
+Components.texts[bottomCounter] = {size: 26, content: "0", opacity: 0.6};
 
 const bottomDetector = 15;
-Components.rulesDetectors[bottomDetector] = {zone: 13, counter: 14};
+Components.rulesDetectors[bottomDetector] = {zone: 13, counter: 17};
 
 const topZone = 16;
 Components.bodies[topZone] = {width: Config.WORLD_WIDTH, height: 20, type: "zone"};
-Components.positions[topZone] = {x: Config.WORLD_WIDTH/2, y: 15};
+Components.positions[topZone] = {x: Config.WORLD_WIDTH/2, y: 25};
 Components.sensors[topZone] = {seeking: 3, detected: false};
 
 const topCounter = 17;
-Components.positions[topCounter] = {x: 50, y: 50};
-Components.texts[topCounter] = {size: 22, content: "0", opacity: 0.6};
+Components.positions[topCounter] = {x: 60, y: 220};
+Components.texts[topCounter] = {size: 26, content: "0", opacity: 0.6};
 
 const topDetector = 18;
-Components.rulesDetectors[topDetector] = {zone: 16, counter: 17};
+Components.rulesDetectors[topDetector] = {zone: 16, counter: 14};
 
 export default Components;
