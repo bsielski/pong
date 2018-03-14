0f9e73537cb83542ae4ac3999ffd19aaeb082043
Strategiusz <strategiusz.yt@gmail.com>
2018-03-14 17:28:41 +0100

    AI knows its direction

--- src/ai.js
+++ src/ai.js
@@ -12,14 +12,21 @@ class AI {
   update(delta) {
     Object.keys(this.ai_components).forEach(aiId => {
       let nearestBall = null;
+      let west = "left";
+      let east = "right";
+      if (this.order_components[aiId].direction % (2 * Math.PI) > Math.PI ) {
+        west = "right";
+        east = "left";
+      }
+
       Object.keys(this.ball_components).forEach(ballId => {
         nearestBall = ballId;
       });
       if (this.position_components[nearestBall].x - 45 > this.position_components[aiId].x) {
-        this.order_components[aiId].movement = "left";
+        this.order_components[aiId].movement = east;
       }
       else if (this.position_components[nearestBall].x + 45 < this.position_components[aiId].x) {
-        this.order_components[aiId].movement = "right";
+        this.order_components[aiId].movement = west;
       }
     });
   }
