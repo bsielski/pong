186765218badf0d31a62f32a9d55058984604de1
Strategiusz <strategiusz.yt@gmail.com>
2018-03-17 18:53:36 +0100

    Make keyboard input independent from components

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,36 @@
 class Controller {
 
   constructor() {
+    this.keyState = {
+      leftArrow: false,
+      rightArrow: false
+    };
+
+    window.addEventListener('keydown', event => {
+      switch (event.keyCode) {
+        case 37: //left
+        // console.log("LEFT");
+        this.keyState.leftArrow = true;
+        break;
+        case 39: //right
+        // console.log("RIGHT");
+        this.keyState.rightArrow = true;
+        break;
+      }
+    });
+    window.addEventListener('keyup', event => {
+      switch (event.keyCode) {
+        case 37: //left
+        // console.log("LEFT UP");
+        this.keyState.leftArrow = false;
+        break;
+        case 39: //right
+        // console.log("RIGHT UP");
+        this.keyState.rightArrow = false;
+        break;
+      }
+    });
+
     this.loadLevel = this.loadLevel.bind(this);
     this.update = this.update.bind(this);
   }
@@ -8,42 +38,15 @@ class Controller {
   loadLevel(components) {
     this.order_components = components.orders;
     this.input_components = components.inputs;
-
-    Object.keys(this.input_components).forEach(id => {
-      window.addEventListener('keydown', (function(event) {
-        switch (event.keyCode) {
-          case 37: //left
-          // console.log("LEFT");
-          this.input_components[id].leftArrow = true;
-          break;
-          case 39: //right
-          // console.log("RIGHT");
-          this.input_components[id].rightArrow = true;
-          break;
-        }
-      }).bind(this));
-      window.addEventListener('keyup', (function(event) {
-        switch (event.keyCode) {
-          case 37: //left
-          // console.log("LEFT UP");
-          this.input_components[id].leftArrow = false;
-          break;
-          case 39: //right
-          // console.log("RIGHT UP");
-          this.input_components[id].rightArrow = false;
-          break;
-        }
-      }).bind(this));
-    });
   }
 
   update(delta) {
     Object.keys(this.input_components).forEach(id => {
-      if (this.input_components[id].leftArrow === true && this.input_components[id].rightArrow === false) {
+      if (this.keyState.leftArrow === true && this.keyState.rightArrow === false) {
         this.order_components[id].movement = "left";
         // console.log("ORDER: left");
       }
-      else if (this.input_components[id].leftArrow === false && this.input_components[id].rightArrow === true) {
+      else if (this.keyState.leftArrow === false && this.keyState.rightArrow === true) {
         this.order_components[id].movement = "right";
         // console.log("ORDER: right");
       }
