8a18850984e5c38fc740c56304fa751c6de33777
Strategiusz <strategiusz.yt@gmail.com>
2018-03-17 10:11:08 +0100

    Delete useless shape types

--- src/components.js
+++ src/components.js
@@ -25,7 +25,7 @@ const Components = {
 }
 
 const enemyPaddleId = uuid();
-Components.shapes[enemyPaddleId] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, angle: 0, type: "stopping"};
+Components.shapes[enemyPaddleId] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, angle: 0};
 Components.bodies[enemyPaddleId] = {};
 Components.stopping[enemyPaddleId] = {};
 Components.collisions[enemyPaddleId] = [];
@@ -36,7 +36,7 @@ Components.orders[enemyPaddleId] = {movement: "stop", direction: Math.PI * 3/2};
 Components.ai[enemyPaddleId] = {};
 
 const playerPaddleId = uuid();
-Components.shapes[playerPaddleId] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, angle: 0, type: "stopping"};
+Components.shapes[playerPaddleId] = {width: Config.PADDLE_WIDTH, height: Config.PADDLE_HEIGHT, angle: 0};
 Components.bodies[playerPaddleId] = {};
 Components.stopping[playerPaddleId] = {};
 Components.collisions[playerPaddleId] = [];
@@ -48,7 +48,7 @@ Components.inputs[playerPaddleId] = {leftArrow: false, rightArrow: false};
 // Components.ai[playerPaddleId] = {};
 
 const ballId = uuid();
-Components.shapes[ballId] = {width: Config.BALL_WIDTH, height: Config.BALL_HEIGHT, angle: 0, type: "bouncing"};
+Components.shapes[ballId] = {width: Config.BALL_WIDTH, height: Config.BALL_HEIGHT, angle: 0};
 Components.bodies[ballId] = {};
 Components.bouncing[ballId] = {};
 Components.collisions[ballId] = [];
@@ -58,28 +58,28 @@ Components.movements[ballId] = {minSpeed: 0.25, speed: 0.4, maxSpeed: 0.55, angl
 Components.balls[ballId] = {};
 
 const leftWallId = uuid();
-Components.shapes[leftWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0, type: "immobile"};
+Components.shapes[leftWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0};
 Components.bodies[leftWallId] = {};
 Components.collisions[leftWallId] = [];
 Components.sprites[leftWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0, image: LOLPIXELS, color: 0xffffff, opacity: 1.0};
 Components.positions[leftWallId] = {x: 0, y: Config.WORLD_HEIGHT/2};
 
 const rightWallId = uuid();
-Components.shapes[rightWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0, type: "immobile"};
+Components.shapes[rightWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0};
 Components.bodies[rightWallId] = {};
 Components.collisions[rightWallId] = [];
 Components.sprites[rightWallId] = {width: 40, height: Config.WORLD_HEIGHT, angle: 0, image: LOLPIXELS, color: 0xffffff, opacity: 1.0};
 Components.positions[rightWallId] = {x: Config.WORLD_WIDTH, y: Config.WORLD_HEIGHT/2};
 
 const toptWallId = uuid();
-Components.shapes[toptWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0, type: "immobile"};
+Components.shapes[toptWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0};
 Components.bodies[toptWallId] = {};
 Components.collisions[toptWallId] = [];
 Components.sprites[toptWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0, image: LOLPIXELS, color: 0xffffff, opacity: 1.0};
 Components.positions[toptWallId] = {x: Config.WORLD_WIDTH/2, y: 0};
 
 const bottomWallId = uuid();
-Components.shapes[bottomWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0, type: "immobile"};
+Components.shapes[bottomWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0};
 Components.bodies[bottomWallId] = {};
 Components.collisions[bottomWallId] = [];
 Components.sprites[bottomWallId] = {width: Config.WORLD_WIDTH, height: 40, angle: 0, image: LOLPIXELS, color: 0xffffff, opacity: 1.0};
@@ -100,21 +100,21 @@ const fpsCounterId = uuid();
 Components.fpsCounters[fpsCounterId] = {variable: fpsNumberId};
 
 const obstacle1Id = uuid();
-Components.shapes[obstacle1Id] = {width: 60, height: 80, angle: 1, type: "immobile"};
+Components.shapes[obstacle1Id] = {width: 60, height: 80, angle: 1};
 Components.bodies[obstacle1Id] = {};
 Components.collisions[obstacle1Id] = [];
 Components.sprites[obstacle1Id] = {width: 60, height: 80, angle: 1, image: LOLPIXELS, color: 0xf42222, opacity: 0.6};
 Components.positions[obstacle1Id] = {x: 20, y: Config.WORLD_HEIGHT/4};
 
 const obstacle2Id = uuid();
-Components.shapes[obstacle2Id] = {width: 80, height: 80, angle: 0.4, type: "immobile"};
+Components.shapes[obstacle2Id] = {width: 80, height: 80, angle: 0.4};
 Components.bodies[obstacle2Id] = {};
 Components.collisions[obstacle2Id] = [];
 Components.sprites[obstacle2Id] = {width: 80, height: 80, angle: 0.4, image: LOLPIXELS, color: 0xf4f2f7f, opacity: 0.6};
 Components.positions[obstacle2Id] = {x: Config.WORLD_WIDTH/2+70, y: Config.WORLD_HEIGHT/3+20};
 
 const obstacle3Id = uuid();
-Components.shapes[obstacle3Id] = {width: 60, height: 30, angle: 2, type: "immobile"};
+Components.shapes[obstacle3Id] = {width: 60, height: 30, angle: 2};
 Components.bodies[obstacle3Id] = {};
 Components.collisions[obstacle3Id] = [];
 Components.sprites[obstacle3Id] = {width: 60, height: 30, angle: 2, image: LOLPIXELS, color: 0x11f2f7f, opacity: 0.6};
@@ -132,7 +132,7 @@ const enemyPointsId = uuid();
 Components.variables[enemyPointsId] = {value: 0};
 
 const bottomZoneId = uuid();
-Components.shapes[bottomZoneId] = {width: Config.WORLD_WIDTH, height: 20, angle: 0, type: "zone"};
+Components.shapes[bottomZoneId] = {width: Config.WORLD_WIDTH, height: 20, angle: 0};
 Components.collisions[bottomZoneId] = [];
 Components.positions[bottomZoneId] = {x: Config.WORLD_WIDTH/2, y: Config.WORLD_HEIGHT - 25};
 Components.touchSensors[bottomZoneId] = {seeking: ballId, last: false, current: false, variable: enemyPointsId, operation: +1};
@@ -145,7 +145,7 @@ const playerPointsId = uuid();
 Components.variables[playerPointsId] = {value: 0};
 
 const topZoneId = uuid();
-Components.shapes[topZoneId] = {width: Config.WORLD_WIDTH, height: 20, angle: 0, type: "zone"};
+Components.shapes[topZoneId] = {width: Config.WORLD_WIDTH, height: 20, angle: 0};
 Components.collisions[topZoneId] = [];
 Components.positions[topZoneId] = {x: Config.WORLD_WIDTH/2, y: 25};
 Components.touchSensors[topZoneId] = {seeking: ballId, last: false, current: false, variable: playerPointsId, operation: +1};
